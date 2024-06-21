a565af0f5bb5cee961b406cf4efb9ec475f96cc6
Deon Menezes <deonmenezescodes@gmail.com>
2024-06-21 22:47:08 +0530

    final commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   return (
-    <NextUIProvider navigate={router.push}>
+    <NextUIProvider>
       <NextThemesProvider>
         <SessionProvider session={pageProps.session}>
           <Component {...pageProps} />
